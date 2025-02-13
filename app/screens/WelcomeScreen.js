9589b6b2b93dcf93740e12ea47445305c3a6a31c
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-12 19:38:56 -0500

    home screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, View} from "react-native";
+import {Image, ImageBackground, StyleSheet, View} from "react-native";
 import AppButton from "../components/AppButton";
 import colors from "../configs/colors";
 
-function LoginScreen(props) {
+function WelcomeScreen(props) {
     return (
-        <ImageBackground  style={styles.background} src={}>
-
+        <ImageBackground
+            style={styles.background}
+            source={ require('../assets/background.jpeg')} blurRadius={7}
+        >
+            <View style={styles.logoContainer}>
+                <Image source={require("../assets/creditmaze-high-resolution-logo-transparent.png")}
+                       resizeMode={"contain"}
+                       style={styles.logo}
+                />
+            </View>
             <View>
                 <AppButton styleButton={styles.loginButton} title={"Login"}
                            onPress={() => console.log("Logged In")}/>
@@ -19,24 +27,33 @@ function LoginScreen(props) {
 }
 
 const styles = StyleSheet.create({
+    logoContainer: {
+        position: "absolute",
+        top: 70,
+        left: 55
+    },
+    logo: {
+        height: 300,
+        width: 300
+    },
     loginButton: {
         backgroundColor: colors.primary,
         borderRadius: 30,
-        height: 35,
+        height: 40,
 
     },
     signupButton: {
         backgroundColor: colors.secondary,
         borderRadius: 30,
-        height: 35,
+        height: 40,
     },
     background: {
-        paddingHorizontal: 20,
+        paddingHorizontal: 15,
         flex: 1,
         flexDirection: "column",
         justifyContent: "flex-end",
-        paddingBottom: 30
+        paddingBottom: 50
     }
 
 })
-export default LoginScreen;
+export default WelcomeScreen;
