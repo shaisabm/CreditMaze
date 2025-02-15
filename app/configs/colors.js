5add6edb13769513b528eb580cbbc4f85012871f
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-14 23:50:34 -0500

    added gray and light gray colors

--- app/configs/colors.js
+++ app/configs/colors.js
@@ -3,5 +3,8 @@ export default {
     primary: '#00FFFF',
     secondary: 'rgb(255,204,3)',
     black: "black",
-    white: "white"
+    white: "white",
+    gray: "#252424",
+    // lightGray: "gray",
+    lightGray: "#757373"
 };
