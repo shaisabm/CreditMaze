63e06ed2a95342d23e7a4f1d800e5d88ebebcedb
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-04 00:50:38 -0500

    enhance AppText component to include default text styling

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text} from "react-native";
 import style from "../configs/defaultStyle";
+import defaultStyle from "../configs/defaultStyle";
 
 function AppText ({children, styles}) {
     return (
-        <Text style={[style.text, styles]} >{children}</Text>
+        <Text style={[style.text, defaultStyle.text, styles]} >{children}</Text>
     );
 }
 
