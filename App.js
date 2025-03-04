10719273084d5e95590f5f8cbd66517394af01f2
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-04 00:49:50 -0500

    added color picker and state management to App.js

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import {StyleSheet, View} from 'react-native';
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import HomeScreen from "./app/screens/HomeScreen";
+import 'react-native-reanimated';
+import Slider from "@react-native-community/slider";
+import ColorPicker from 'react-native-wheel-color-picker';
+import {useState} from "react";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <HomeScreen/>
-        </View>
+  const [color, setColor] = useState('#FF0000'); // Start with a default color
 
-    );
+  const onColorChange = color => {
+    setColor(color);
+  };
+
+  return (
+       <HomeScreen/>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-
-    }
+  sectionContainer: {
+    flex: 1,
+    marginTop: 70,
+    paddingHorizontal: 24,
+  },
 });
