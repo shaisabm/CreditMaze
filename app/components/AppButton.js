cb4f7d13ac58a390216825e4ab0f68554a1d28b6
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-04 00:50:26 -0500

    refactor AppButton.js to simplify AppText styling and remove unused imports

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, View} from "react-native";
+import {StyleSheet, TouchableOpacity} from "react-native";
 import AppText from "./AppText";
-import defaultStyle from "../configs/defaultStyle";
 
 
 function AppButton({title, onPress, styleButton, styleText}) {
     return (
         <TouchableOpacity style={[styles.container, styleButton]} onPress={onPress}>
-            <AppText styles={[styleText, defaultStyle.text, styles.text]}>{title}</AppText>
+            <AppText styles={[styles.text, styleText]}>{title}</AppText>
         </TouchableOpacity>
 
     );
