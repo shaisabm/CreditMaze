3024107f7709e6b4f4a8666c07b6589d998664c1
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-04 00:51:27 -0500

    update defaultStyle.js to adjust font size and add text color

--- app/configs/defaultStyle.js
+++ app/configs/defaultStyle.js
@@ -1,8 +1,10 @@
 import {Platform} from "react-native";
+import colors from "./colors";
 
 export default {
     text: {
         fontFamily: Platform.OS === 'ios'? 'Avenir': 'Roboto',
-        fontSize: 18
+        fontSize: 15,
+        color: colors.white
     }
 }
