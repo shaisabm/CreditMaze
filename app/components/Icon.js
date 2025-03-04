10ac9f6429226f83e8ee3e358b7297108d783e69
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-04 00:51:53 -0500

    enhance Icon component to support onPress functionality and display title

--- app/components/Icon.js
+++ app/components/Icon.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import {StyleSheet, View} from "react-native";
+import {StyleSheet, TouchableOpacity, View} from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import AppText from "./AppText";
 
-function Icon ({name, color, backgroundColor, size=40, style}) {
+function Icon({name, color, title, backgroundColor, size = 40, style, onPress}) {
     return (
 
-        <View style={[styles.container, {backgroundColor, height: size*1.8, width: size*1.8}, style]}>
-            <MaterialCommunityIcons name={name} size={size} color={color}/>
-        </View>
+        <TouchableOpacity onPress={onPress}>
+            <View style={[styles.container, {backgroundColor, height: size * 1.8, width: size * 1.8}, style]}>
+                <MaterialCommunityIcons name={name} size={size} color={color}/>
+                {title && <AppText styles={{fontSize: size*0.50}}>{title}</AppText>}
+            </View>
+        </TouchableOpacity>
+
     );
 }
 
-const styles= StyleSheet.create({
-        container: {
-            // height: 80,
-            // width: 80,
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: "50%"
+const styles = StyleSheet.create({
+    container: {
+        // height: 80,
+        // width: 80,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "50%"
 
     }
 })
