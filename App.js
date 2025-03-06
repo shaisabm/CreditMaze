92f6ae29617eebbc2dcc3bd7c95a289afe0d844c
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-06 01:08:54 -0500

    refactor App.js to integrate Modal and improve layout with updated styles

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
-import {StyleSheet, View} from 'react-native';
+import {Modal, StyleSheet, View} from 'react-native';
 import HomeScreen from "./app/screens/HomeScreen";
 import 'react-native-reanimated';
 import Slider from "@react-native-community/slider";
 import ColorPicker from 'react-native-wheel-color-picker';
-import {useState} from "react";
+import React, {useState} from "react";
+import AppNavBar from "./app/components/AppNavBar";
+import Icon from "./app/components/Icon";
+import colors from "./app/configs/colors";
+import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
+import Overlay from "./app/components/Overlay";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [color, setColor] = useState('#FF0000'); // Start with a default color
 
-  const onColorChange = color => {
-    setColor(color);
-  };
+    const [visible, setVisible] = useState(false)
 
-  return (
-       <HomeScreen/>
-  );
+    return (
+        <>
+            {/*<Screen>*/}
+
+            {/*    <Overlay animationType={'fade'} visible={visible} style={{backgroundColor: "gray"}}>*/}
+            {/*        <View style={styles.sectionContainer}>*/}
+
+            {/*            <AppButton title={"click"} onPress={() => setVisible(!visible)} styleText={{color: "red"}}/>*/}
+            {/*        </View>*/}
+            {/*    </Overlay>*/}
+
+            {/*    <View style={styles.sectionContainer}>*/}
+
+            {/*        <AppButton title={"click"} onPress={() => setVisible(!visible)} styleText={{color: colors.black}}/>*/}
+            {/*    </View>*/}
+            {/*</Screen>*/}
+            <HomeScreen/>
+
+        </>
+
+    );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    flex: 1,
-    marginTop: 70,
-    paddingHorizontal: 24,
-  },
+    sectionContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+
+
+    },
 });
